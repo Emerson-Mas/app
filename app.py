699171b67ba253f7444c37dadf56520380a769c1
Emerson-Mas <emersonmas@protonmail.com>
2025-01-22 17:56:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,10 @@ def index():
     }
     return jsonify(data)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Usa a variável $PORT ou 5000 como fallback
+    app.run(debug=False, host="0.0.0.0", port=port)
+
 
